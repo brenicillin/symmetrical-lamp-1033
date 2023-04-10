08db8874b25344f183431be8fe355df9009c1278
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-10 13:10:20 -0400

    add add_items div for precise testing

--- spec/features/customer/show_spec.rb
+++ spec/features/customer/show_spec.rb
@@ -1,39 +1,17 @@
-# Story 1
-
-# As a visitor, 
-# When I visit a customer show page,
-# I see the customer's name,
-# And I see its a list of its items
-# including the item's name, price, and the name of the supermarket that it belongs to.
-
-# Story 2
-
-# As a visitor,
-# When I visit a customer's show page,
-# Then I see a form to add an item to this customer.
-# When I fill in a field with the id of an existing item,
-# And I click submit,
-# Then I am redirected back to the customer's show page, 
-# And I see the item now listed under this customer's items.
-# (You do not have to test for a sad path, for example if the ID submitted is not an existing item)
 require 'rails_helper'
 
 RSpec.describe 'Customer Show Page' do
   before(:each) do
-    @supermarket = Supermarket.create!(name: "Publix", location: "123 Main Street")
-    @item_1 = @supermarket.items.create!(name: "Milk", price: 3)
-    @item_2 = @supermarket.items.create!(name: "Eggs", price: 13)
-    @item_3 = @supermarket.items.create!(name: "Bread", price: 5)
-    @customer = Customer.create!(name: "Bob")
-    @customer.items << @item_1
-    @customer.items << @item_2
+    test_data
+    @customer_1.items << @item_1
+    @customer_1.items << @item_2
   end
 
   describe 'As a visitor' do
     it 'I can see a customer and their items' do
-      visit "/customers/#{@customer.id}"
+      visit "/customers/#{@customer_1.id}"
       
-      expect(page).to have_content(@customer.name)
+      expect(page).to have_content(@customer_1.name)
       within "#item_#{@item_1.id}" do
         expect(page).to have_content(@item_1.name)
         expect(page).to have_content(@item_1.price)
@@ -47,12 +25,16 @@ RSpec.describe 'Customer Show Page' do
     end
 
     it 'I can add an item to a customer' do
-      visit "/customers/#{@customer.id}"
+      visit "/customers/#{@customer_1.id}"
       expect(page).to_not have_content("Bread")
 
-      fill_in :item_id, with: @item_3.id
-      click_button "Submit"
-
+      within '#add_items' do
+        expect(page).to have_field(:item_id)
+        expect(page).to have_button("Submit")
+        fill_in :item_id, with: @item_3.id
+        click_button "Submit"
+      end
+      
       expect(page).to have_content("Bread")
     end
   end
