40b2a6c7b7453ed7e62d9c5cc92e5de5bb9a6804
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-10 13:11:38 -0400

    add extension test

---
+++ spec/features/supermarket/show_spec.rb
@@ -0,0 +1,23 @@
+# Extension
+
+# As a visitor,
+# When I visit a supermarket's show page,
+# Then I see a unique list of all customers that have shopped at the supermarket.
+require 'rails_helper'
+
+RSpec.describe 'Supermarket Show Page' do
+  before(:each) do
+    test_data
+    @customer_1.items << @item_1
+    @customer_1.items << @item_2
+    @customer_2.items << @item_1
+    @customer_2.items << @item_3
+  end
+  describe 'As a visitor' do
+    it 'I can see a list of all customers' do
+      visit "/supermarkets/#{@supermarket.id}"
+      expect(page).to have_content(@customer_1.name)
+      expect(page).to have_content(@customer_2.name)
+    end
+  end
+end
