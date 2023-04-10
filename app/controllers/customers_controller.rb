d06e07ff8b360899fa5fc9bd26eb106dd7fb45de
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-10 12:04:44 -0400

    add add_item action to customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,11 @@ class CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+
+  def add_item
+    @customer = Customer.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @customer.items << @item
+    redirect_to "/customers/#{@customer.id}"
+  end
 end
