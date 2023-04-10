6542af6144e619f4f131a7d07ac64d8e2b78b92c
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-10 12:04:54 -0400

    add patch route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/customers/:id', to: 'customers#show'
-  
+  patch '/customers/:id', to: 'customers#add_item'
+
 end
