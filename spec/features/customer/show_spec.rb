77def61f16316384eb8e567d6373390081c17e7b
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-10 11:45:05 -0400

    change let block to before each block

--- spec/features/customer/show_spec.rb
+++ spec/features/customer/show_spec.rb
@@ -5,21 +5,39 @@
 # I see the customer's name,
 # And I see its a list of its items
 # including the item's name, price, and the name of the supermarket that it belongs to.
+
+# Story 2
+
+# As a visitor,
+# When I visit a customer's show page,
+# Then I see a form to add an item to this customer.
+# When I fill in a field with the id of an existing item,
+# And I click submit,
+# Then I am redirected back to the customer's show page, 
+# And I see the item now listed under this customer's items.
+# (You do not have to test for a sad path, for example if the ID submitted is not an existing item)
 require 'rails_helper'
 
 RSpec.describe 'Customer Show Page' do
-  let(:customer) { Customer.create!(name: 'Bob') }
-  let(:supermarket) { Supermarket.create!(name: 'Publix', location: '123 Main St') }
-  let(:item) { Item.create!(name: 'Milk', price: 3, supermarket: supermarket) }
-  
+  before(:each) do
+    @supermarket = Supermarket.create!(name: "Publix", location: "123 Main Street")
+    @item_1 = @supermarket.items.create!(name: "Milk", price: 3)
+    @item_2 = @supermarket.items.create!(name: "Eggs", price: 13)
+    @customer = Customer.create!(name: "Bob")
+    @customer.items << @item_1
+    @customer.items << @item_2
+  end
+
   describe 'As a visitor' do
     it 'I can see a customer and their items' do
-      visit "/customers/#{customer.id}"
-
-      expect(page).to have_content(customer.name)
-      expect(page).to have_content(item.name)
-      expect(page).to have_content(item.price)
-      expect(page).to have_content(item.supermarket.name)
+      visit "/customers/#{@customer.id}"
+      save_and_open_page
+      expect(page).to have_content(@customer.name)
+      expect(page).to have_content("Milk")
+      expect(page).to have_content("Eggs")
+      expect(page).to have_content("Cost USD: $3")
+      expect(page).to have_content("Cost USD: $13")
+      expect(page).to have_content("Available at: Publix")
     end
   end
 end
